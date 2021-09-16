7af01eb6bb48cc53fe8d2458543be51a1bedaf85
clank <84193593+clank-bot@users.noreply.github.com>
2021-09-16 17:51:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var canvas = new fabric.Canvas('myCanvas');
  var x= document.getElementById("myAudio");
 
-function new_image(get_image)
+function new_image()
 {
-fabric.Image.fromURL('get_image', function(Img) {
+fabric.Image.fromURL('BirthdayImage.jpg', function(Img) {
     block_image_object = Img;
 
     block_image_object.scaleToWidth(700);
